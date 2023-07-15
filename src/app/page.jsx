bff148df0014723a2cc8843ca1be5d9eb0f708d8
oussamaelhousni <oussamaelhousnioe@gmail.com>
2023-07-15 14:27:49 +0100

    building home page

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,3 +1,20 @@
+import Link from "next/link";
+import Image from "next/image";
+import styles from "./page.module.css";
 export default function Home() {
-  return <h1>Hello From JSX</h1>;
+  return (
+    <section className={styles.hero}>
+      <div className={styles.hero_text}>
+        <h1>Better design for your digital products</h1>
+        <p>
+          Turing your ideas into reality,We bring together the teams from the
+          global industry
+        </p>
+        <Link href="/">See our works</Link>
+      </div>
+      <div className={styles.hero_img}>
+        <Image src="/hero.png" layout="fill" alt="hero image" />
+      </div>
+    </section>
+  );
 }
--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,9 +1,22 @@
+import Image from "next/image";
 import styles from "./footer.module.css";
 const Footer = () => {
   return (
     <footer className={styles.footer}>
       <p>Made with ❤️ by Oussama Elhousni</p>
-      <div></div>
+      <div className={styles.socials}>
+        {new Array(1, 2, 3, 4).map((val) => {
+          return (
+            <Image
+              src={`/${val}.png`}
+              width="20"
+              height="20"
+              alt="social"
+              className={styles.icon}
+            />
+          );
+        })}
+      </div>
     </footer>
   );
 };
